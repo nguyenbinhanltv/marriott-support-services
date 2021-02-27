873c5d93c0bfd65702da4ecca0cc51424682b03a
Nguyen Binh An <nguyenbinhanltv@gmail.com>
2021-02-28 00:52:52 +0700

    update auth + inauth guard

--- marriott/src/app/core/guards/inauth.guard.ts
+++ marriott/src/app/core/guards/inauth.guard.ts
@@ -1,35 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { AuthenticationService } from '../../core/services/authentication.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanActivateChild {
+export class InauthGuard implements CanActivate, CanActivateChild {
 
-  constructor(private authService: AuthenticationService, private router: Router) {
+  constructor(private authService: AuthenticationService) {
 
   }
-
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authService.getAdmin) {
+    if (this.authService.getAdmin == null) {
       return of(true);
     }
 
-    this.router.navigateByUrl("login");
     return of(false);
   }
   canActivateChild(
     childRoute: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authService.getAdmin) {
+    if (this.authService.getAdmin == null) {
       return of(true);
     }
 
-    this.router.navigateByUrl("login");
     return of(false);
   }
 
